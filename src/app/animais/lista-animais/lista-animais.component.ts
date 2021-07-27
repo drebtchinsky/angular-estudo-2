376d1508a933f64301036111e34f13506f29f969
Gabriel Drebtchinsky <gabrieldqcarvalho@gmail.com>
2021-07-27 00:14:30 -0300

    lista de animais

--- src/app/animais/lista-animais/lista-animais.component.ts
+++ src/app/animais/lista-animais/lista-animais.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { AnimaisService } from './../animais.service';
+import { UsuarioService } from './../../autenticacao/usuario/usuario.service';
+import { Animais } from './../animais';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-lista-animais',
   templateUrl: './lista-animais.component.html',
-  styleUrls: ['./lista-animais.component.css']
+  styleUrls: ['./lista-animais.component.css'],
 })
 export class ListaAnimaisComponent implements OnInit {
+  animais: Animais = [];
 
-  constructor() { }
+  constructor(
+    private usuarioService: UsuarioService,
+    private animaisService: AnimaisService
+  ) {}
 
   ngOnInit(): void {
+    this.usuarioService.retornaUsuario().subscribe((usuario) => {
+      const userName = usuario.name ?? '';
+      this.animaisService.listaDoUsuario(userName).subscribe((animais) => {
+        this.animais = animais;
+      });
+    });
   }
-
 }
